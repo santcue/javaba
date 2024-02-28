114fe2ef1527e4deae98cd16c0acad4330ee74c5
santcue <sardila2225@cue.edu.co>
2024-02-27 21:30:55 -0500

    first commit

---
+++ src/main/java/cue/edu/co/Main.java
@@ -0,0 +1,27 @@
+package cue.edu.co;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "root123";
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement =  connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM productos");
+            ) {
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id"));
+                System.out.print("|");
+                System.out.println(resultSet.getString("nombre"));
+                System.out.print("|");
+                System.out.println(resultSet.getString("precio"));
+                System.out.print("|");
+                System.out.println(resultSet.getString("fecha_registro"));
+            }
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
